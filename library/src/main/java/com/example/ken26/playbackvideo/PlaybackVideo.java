9d11bf24e0a54727cd8d284357a91219f9355d6c
kenjiro kariya <kandroid51@gmail.com>
2016-03-27 11:24:41 +0900

    add codes to playback video
     change build.gradle from applicatoin to library and remove applicationId

--- library/src/main/java/com/example/ken26/playbackvideo/PlaybackVideo.java
+++ library/src/main/java/com/example/ken26/playbackvideo/PlaybackVideo.java
@@ -1,7 +1,12 @@
 package com.example.ken26.playbackvideo;
 
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.VideoView;
 
 public class PlaybackVideo extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class PlaybackVideo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playback_video);
+
+        Button button;
+
+        button = (Button) findViewById(R.id.button);
+
+        getWindow().setFormat(PixelFormat.UNKNOWN);
+        Intent intent = getIntent();
+        Uri uri = Uri.parse(intent.getStringExtra("VideoFile"));
+        VideoView mVideoPlay = (VideoView) findViewById(R.id.VideoView);
+
+        mVideoPlay.setVideoURI(uri);
+        mVideoPlay.requestFocus();
+        mVideoPlay.start();
     }
 }
